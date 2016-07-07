de9c154a7e1677a82b7b46a4370c90fb423d4c5c
David Neves <David Neves>
2016-07-07 18:29:24 +0100

    LibGdx build

---
+++ desktop/src/org/academiadecodigo/std/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.std.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.std.STDIsABits;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new STDIsABits(), config);
+	}
+}
