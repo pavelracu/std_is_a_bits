22f5b6eb8be62ff024453ee2ab46e694bd515860
davidneves <neves.davidpereira@gmail.com>
2016-07-12 12:03:11 +0100

    Updated Hub

--- core/src/org/academiadecodigo/std/screens/PlayScreen.java
+++ core/src/org/academiadecodigo/std/screens/PlayScreen.java
@@ -13,7 +13,6 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -45,13 +44,14 @@ public class PlayScreen implements Screen {
     private OrthogonalTiledMapRenderer renderer;
 
     private World world;
-    private Box2DDebugRenderer b2dr;
     private B2WorldCreator creator;
 
     private Player player1;
     private Player player2;
 
     private Cell cell;
+
+    private float gameOverTimer;
 
     public PlayScreen(Tumor game, AssetManager manager) {
 
@@ -85,6 +85,8 @@ public class PlayScreen implements Screen {
         world.setContactListener(new WorldContactListener(manager));
 
         controller = new Controller(game);
+
+        gameOverTimer = 0;
 
     }
 
@@ -132,39 +134,48 @@ public class PlayScreen implements Screen {
     public void update(float dt) {
 
         if (hud.isTimeUp() || isGameOver()) {
-            gameOver();
-        }
 
-        handleInput(dt);
+            hud.gameOver();
 
-        world.step(1 / 60f, 6, 2);
+            if (gameOverTimer > 5) {
+                gameOver();
+            } else {
+                gameOverTimer += dt;
+            }
+        } else {
 
-        hud.clearScore();
-        for (Cell cell : creator.getCells()) {
-            Shape shape = cell.getB2Body().getFixtureList().get(0).getShape();
-            shape.setRadius(20 / Tumor.PPM);
-            cell.update(dt);
-            hud.addScore(1, cell.getState());
+            handleInput(dt);
 
-        }
+            world.step(1 / 60f, 6, 2);
 
-        if (MathUtils.random(100) < 10) {
+            hud.clearScore();
             for (Cell cell : creator.getCells()) {
-                cell.getB2Body().applyForceToCenter(new Vector2(MathUtils.random(-1f, 1f), MathUtils.random(-1f, 1f)), true);
-                if (MathUtils.random(100) < 10) {
+                Shape shape = cell.getB2Body().getFixtureList().get(0).getShape();
+                shape.setRadius(20 / Tumor.PPM);
+                cell.update(dt);
+                hud.addScore(1, cell.getState());
 
-                    Shape shape = cell.getB2Body().getFixtureList().get(0).getShape();
-                    shape.setRadius(25 / Tumor.PPM);
+            }
+
+            if (MathUtils.random(100) < 10) {
+                for (Cell cell : creator.getCells()) {
+                    cell.getB2Body().applyForceToCenter(new Vector2(MathUtils.random(-1f, 1f), MathUtils.random(-1f, 1f)), true);
+                    if (MathUtils.random(100) < 10) {
+
+                        Shape shape = cell.getB2Body().getFixtureList().get(0).getShape();
+                        shape.setRadius(25 / Tumor.PPM);
+                    }
                 }
             }
+
+            player1.update(dt);
+            player2.update(dt);
+
+            hud.update(dt);
+
+            renderer.setView(gameCam);
+
         }
-
-        player1.update(dt);
-        player2.update(dt);
-
-        hud.update(dt);
-
-        renderer.setView(gameCam);
     }
 
     public boolean isGameOver() {
@@ -177,6 +188,11 @@ public class PlayScreen implements Screen {
     }
 
     private void gameOver() {
+
+
+        hud.gameOver();
+
+
 
         game.setScreen(new GameOverScreen(game, manager));
 
@@ -210,7 +226,7 @@ public class PlayScreen implements Screen {
 
         game.sb.end();
 
-        if(Gdx.app.getType() == Application.ApplicationType.Android) {
+        if (Gdx.app.getType() == Application.ApplicationType.Android) {
             controller.draw();
         }
 
@@ -247,7 +263,6 @@ public class PlayScreen implements Screen {
         map.dispose();
         renderer.dispose();
         world.dispose();
-        //b2dr.dispose();
         hud.dispose();
     }
 
